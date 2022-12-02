b1a31773d39850337872846ad5b36f5309b4a228
Kirsten Allen <kirsten.allen@lego.com>
2022-12-02 17:33:00 +0000

    chore: Add Apollo & React App

---
+++ pokemon/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { client } from "./ApolloClient/client";
+import { ApolloProvider } from "@apollo/client";
+import Homepage from "./Home";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Homepage />
+    </ApolloProvider>
+  );
+}
+
+export default App;
